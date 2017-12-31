7827a0d056befc6b8d9dcbad563908fd8428628f
bryanjsanchez <bryanjsanchez@gmail.com>
2017-12-31 00:00:14 -0500

    Add javadoc comments

--- src/pyramid/Member.java
+++ src/pyramid/Member.java
@@ -1,13 +1,16 @@
 package pyramid;
 import java.util.ArrayList;
 
+/** Represents a member of the pyramid network.
+ * @author Bryan J Sanchez
+ */
+
 public class Member {
 	private String name;
 	private int illegalAsset;
 	private Member sponsor;
 	private Member mentor;
 	private ArrayList<Member> children;
-	private boolean isArrested = false;
 	
 	public Member (String name, int illegalAsset, Member sponsor) {
 		this.name = name;
@@ -15,34 +18,44 @@ public class Member {
 		this.sponsor = sponsor;
 	}
 
+	/**
+	 * @return Returns member's mentor.
+	 */
 	public Member getMentor() {
 		return mentor;
 	}
-
+	
+	/**
+	 * @param mentor Member's mentor.
+	 */
 	public void setMentor(Member mentor) {
 		this.mentor = mentor;
 	}
 
-	public boolean isArrested() {
-		return isArrested;
-	}
-
-	public void arrest() {
-		this.isArrested = true;
-	}
-
+	/**
+	 * @return Returns member's name.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @return Returns member's illegal asset.
+	 */
 	public int getIllegalAsset() {
 		return illegalAsset;
 	}
 
+	/**
+	 * @return Returns member's sponsor.
+	 */
 	public Member getSponsor() {
 		return sponsor;
 	}
 
+	/**
+	 * @return Returns member's children.
+	 */
 	public ArrayList<Member> getChildren() {
 		return children;
 	}
--- src/pyramid/PyramidNetwork.java
+++ src/pyramid/PyramidNetwork.java
@@ -2,6 +2,9 @@ package pyramid;
 
 import java.util.ArrayList;
 
+/** Represents the hierarchical structure of the pyramid scheme.
+ * @author Bryan J Sanchez
+ */
 public class PyramidNetwork {
 	private Member greatBoss;
 	private ArrayList<Member> members;
